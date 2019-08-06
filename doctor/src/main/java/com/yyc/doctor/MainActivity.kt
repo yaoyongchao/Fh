5ab8ae79761f3776c290b95b0ec8309487a735d2
andy <yongchao.yao@aliyun.com>
2019-08-07 06:46:25 +0800

    MainActivity添加BaseActivity。

--- doctor/src/main/java/com/yyc/doctor/MainActivity.kt
+++ doctor/src/main/java/com/yyc/doctor/MainActivity.kt
@@ -2,11 +2,23 @@ package com.yyc.doctor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.lottchina.baselib.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity() {
+    override fun layoutId(): Int {
+        return R.layout.activity_main
     }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initListener() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
